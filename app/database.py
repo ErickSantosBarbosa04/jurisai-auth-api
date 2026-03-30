dd92a61f27e68e2401445ce06e0fbc3e967c96b2
ErickSantosBarbosa04 <ericksantosbarbosa2004@gmail.com>
2026-03-30 09:17:11 -0300

    database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql+psycopg://postgres:postgres123@localhost:5432/jurisai"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL não foi definida nas variáveis de ambiente!")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
